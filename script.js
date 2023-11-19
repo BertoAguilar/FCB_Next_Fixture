6150c26918d9c0489443f64af96b2de88c7fd998
BertoAguilar <150314027+BertoAguilar@users.noreply.github.com>
2023-11-19 10:44:57 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Visca Barca");
+
+function start(player) {
+  let pos1 = document.querySelector(player);
+  pos1.style.color = "green";
+}
+
+function bench(player) {
+  let pos1 = document.querySelector(player);
+  pos1.style.color = "red";
+}
